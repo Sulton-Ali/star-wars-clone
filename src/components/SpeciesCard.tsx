f417a0515cc85a687c232af7bbdb9e809bdcb56c
Jalolov Sultonali <s.jalolov@hlsys.uz>
2021-12-24 18:43:59 +0500

    render species page

--- src/components/SpeciesCard.tsx
+++ src/components/SpeciesCard.tsx
@@ -14,7 +14,63 @@ const SpeciesCard = (props: ISpeciesCardProps) => {
       <div className="species-card__img">
         <img src={`${IMAGE_API_BASE_URL}species/${specyNumber}.jpg`} alt="" />
       </div>
-      <div className="species-card__info"></div>
+      <div className="species-card__info">
+        <div className="species-card__specs">
+          <div className="species-card__specs-item">
+            <span className="species-card__specs-key">Classification:</span>
+            <span className="species-card__specs-value">
+              {specy.classification}
+            </span>
+          </div>
+
+          <div className="species-card__specs-item">
+            <span className="species-card__specs-key">Language:</span>
+            <span className="species-card__specs-value">{specy.language}</span>
+          </div>
+
+          <div className="species-card__specs-item">
+            <span className="species-card__specs-key">Designation:</span>
+            <span className="species-card__specs-value">
+              {specy.classification}
+            </span>
+          </div>
+
+          <div className="species-card__specs-item">
+            <span className="species-card__specs-key">Average Height:</span>
+            <span className="species-card__specs-value">
+              {specy.average_height}
+            </span>
+          </div>
+
+          <div className="species-card__specs-item">
+            <span className="species-card__specs-key">Average Lifespan:</span>
+            <span className="species-card__specs-value">
+              {specy.average_lifespan}
+            </span>
+          </div>
+
+          <div className="species-card__specs-item">
+            <span className="species-card__specs-key">Eye Colors:</span>
+            <span className="species-card__specs-value">
+              {specy.eye_colors}
+            </span>
+          </div>
+
+          <div className="species-card__specs-item">
+            <span className="species-card__specs-key">Hair Colors:</span>
+            <span className="species-card__specs-value">
+              {specy.hair_colors}
+            </span>
+          </div>
+
+          <div className="species-card__specs-item">
+            <span className="species-card__specs-key">Skin Colors:</span>
+            <span className="species-card__specs-value">
+              {specy.skin_colors}
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
